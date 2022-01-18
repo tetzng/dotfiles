10b2adaaa07c34b192f6e9ad5faf0e49f2225cff
Teppei Taguchi <tp.237.335@gmail.com>
2022-01-18 18:18:57 +0900

    add asdf

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 # peco
 set fish_plugins theme peco
 
+# asdf
+source /usr/local/opt/asdf/libexec/asdf.fish
+
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
