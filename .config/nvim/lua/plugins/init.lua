ee00b194cc96ccb184c3f6510757968691358b2e
Teppei Taguchi <tp.237.335@gmail.com>
2023-02-06 00:00:00 +0900

    update notify options

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -155,13 +155,20 @@ return {
       },
     },
     opts = {
-      timeout = 3000,
+      timeout = 5000,
       max_height = function()
         return math.floor(vim.o.lines * 0.75)
       end,
       max_width = function()
         return math.floor(vim.o.columns * 0.75)
       end,
+      icons = {
+        ERROR = "",
+        WARN = "",
+        INFO = "",
+        DEBUG = "",
+        TRACE = "✎",
+      },
     },
   },
 
