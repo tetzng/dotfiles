f64e0241496e6b69f0c0ec22ef3c1fb7bd99c6d0
Teppei Taguchi <tetzng.tt@gmail.com>
2023-02-26 00:00:00 +0900

    add vscode keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,45 +1,49 @@
-require('keymaps')
-require('options')
+require("keymaps")
+require("options")
 
--- lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable',
-      lazypath,
-  })
+if vim.g.vscode then
+	require("vscode/keymaps")
+else
+	-- lazy.nvim
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable",
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	require("lazy").setup({
+		spec = {
+			{ import = "plugins" },
+		},
+		defaults = {
+			lazy = true,
+			version = "*",
+		},
+		ui = {
+			icons = {
+				cmd = "⌘",
+				config = "🛠",
+				event = "📅",
+				ft = "📂",
+				init = "⚙",
+				keys = "🗝",
+				plugin = "🔌",
+				runtime = "💻",
+				source = "📄",
+				start = "🚀",
+				task = "📌",
+				lazy = "󿦱",
+			},
+		},
+	})
 end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    defaults = {
-        lazy = true,
-        version = "*",
-    },
-    ui = {
-        icons = {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            source = "📄",
-            start = "🚀",
-            task = "📌",
-            lazy = "💤 ",
-        },
-    },
-})
 
 -- Fern
 vim.keymap.set({ 'n' }, '<Leader>e', '<Cmd>Fern . -drawer -toggle<CR>')
---
+++ .config/nvim/lua/vscode/keymaps.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<C-p>", "<<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>>", { silent = true })
