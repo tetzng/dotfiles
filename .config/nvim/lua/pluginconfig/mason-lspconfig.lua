0a2ab229bd40a1bcdef03db2f5d32acc0e8ef4c5
Teppei Taguchi <tetzng.tt@gmail.com>
2023-03-01 00:00:00 +0900

    update mason-lspconfig

--- .config/nvim/lua/pluginconfig/mason-lspconfig.lua
+++ .config/nvim/lua/pluginconfig/mason-lspconfig.lua
@@ -2,6 +2,31 @@ require('mason-lspconfig').setup({
     ensure_installed = {
         'tsserver',
         'eslint',
+        'lua_ls',
+        'gopls',
+        'ruby_ls',
+        'graphql',
+        'tailwindcss',
     },
     automatic_installation = true,
+})
+
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("mason-lspconfig").setup_handlers({
+    function(server_name)
+        lspconfig[server_name].setup({ capabilities = capabilities })
+    end,
+    ["lua_ls"] = function()
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+    end,
 })
