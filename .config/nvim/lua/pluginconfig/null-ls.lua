420534ba26ef4e427ed0e42793db7276484eb1b8
Teppei Taguchi <tetzng.tt@gmail.com>
2023-04-14 03:41:37 +0900

    update

--- .config/nvim/lua/pluginconfig/null-ls.lua
+++ .config/nvim/lua/pluginconfig/null-ls.lua
@@ -9,94 +9,103 @@ local formatting = null_ls.builtins.formatting
 -- completion sources
 local completion = null_ls.builtins.completion
 
+local with_root_file = function(...)
+  local files = { ... }
+  return function(utils)
+    return utils.root_has_file(files)
+  end
+end
+
 local sources = {
-    -- markdown
-    -- diagnostics.markdownlint,
-    -- formatting.markdownlint,
+  -- markdown
+  diagnostics.markdownlint,
+  formatting.markdownlint,
 
-    -- lua
-    formatting.stylua.with({
-        condition = function()
-          return vim.fn.executable("stylua") > 0
-        end,
-    }),
-    -- diagnostics.selene.with({
-    --     condition = function(utils)
-    --       return vim.fn.executable("selene") > 0 and utils.root_has_file({ "selene.toml" })
-    --     end,
-    -- }),
-    -- python
-    -- diagnostics.flake8,
-    -- formatting.black,
-    -- formatting.isort,
+  -- lua
+  formatting.stylua.with({
+    condition = with_root_file(".stylua.toml"),
+  }),
+  diagnostics.selene.with({
+    condition = with_root_file("selene.toml"),
+  }),
 
-    -- ruby
-    diagnostics.rubocop.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".rubocop.yml" })
-        end,
-        -- command = "bundle",
-        -- args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
-    }),
-    formatting.rubocop.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".rubocop.yml" })
-        end,
-        -- command = "bundle",
-        args = { "-A", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
-        -- vim.list_extend(
-        -- 	{ "exec", "rubocop" },
-    }),
+  -- python
+  diagnostics.flake8,
+  formatting.black,
+  formatting.isort,
 
-    diagnostics.eslint.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".eslintrc.js" })
-        end,
-        command = "eslint",
-        args = vim.list_extend(
-            { "--stdin", "--stdin-filename", "$FILENAME" },
-            null_ls.builtins.diagnostics.eslint._opts.args
-        ),
-    }),
-    formatting.eslint.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".eslintrc.js" })
-        end,
-        command = "eslint",
-        args = vim.list_extend(
-            { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
-            null_ls.builtins.formatting.eslint._opts.args
-        ),
-    }),
+  -- ruby
+  diagnostics.rubocop.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".rubocop.yml" })
+    end,
+    -- command = "bundle",
+    -- args = vim.list_extend({ "exec", "rubocop" }, null_ls.builtins.diagnostics.rubocop._opts.args),
+  }),
+  formatting.rubocop.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".rubocop.yml" })
+    end,
+    -- command = "bundle",
+    args = { "-A", "-f", "quiet", "--stderr", "--stdin", "$FILENAME" },
+    -- vim.list_extend(
+    -- 	{ "exec", "rubocop" },
+  }),
 
-    -- yaml
-    diagnostics.yamllint,
-    formatting.yamlfmt,
+  diagnostics.eslint.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".eslintrc.js" })
+    end,
+    command = "eslint",
+    args = vim.list_extend(
+      { "--stdin", "--stdin-filename", "$FILENAME" },
+      null_ls.builtins.diagnostics.eslint._opts.args
+    ),
+  }),
+  formatting.eslint.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".eslintrc.js" })
+    end,
+    command = "eslint",
+    args = vim.list_extend(
+      { "--fix-to-stdout", "--stdin", "--stdin-filename", "$FILENAME" },
+      null_ls.builtins.formatting.eslint._opts.args
+    ),
+  }),
 
-    -- go
-    formatting.gofmt,
+  -- yaml
+  diagnostics.yamllint,
+  formatting.yamlfmt,
 
-    -- rust
-    formatting.rustfmt,
+  -- go
+  formatting.gofmt,
 
-    -- spell
-    completion.spell,
-    -- fish
-    diagnostics.fish.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".config/fish/config.fish" })
-        end,
-    }),
-    -- prettier
-    formatting.prettier.with({
-        condition = function(utils)
-          return utils.root_has_file({ ".prettierrc" })
-        end,
-        -- prefer_local = "node_modules/.bin",
-    }),
+  -- rust
+  formatting.rustfmt,
+
+  -- spell
+  completion.spell,
+  -- fish
+  diagnostics.fish.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".config/fish/config.fish" })
+    end,
+  }),
+  -- prettier
+  formatting.prettier.with({
+    condition = function(utils)
+      return utils.root_has_file({ ".prettierrc" })
+    end,
+    -- prefer_local = "node_modules/.bin",
+  }),
 }
 
-null_ls.setup({
-    debug = false,
+local M = {}
+M.setup = function(on_attach)
+  null_ls.setup({
+    -- debug = true,
     sources = sources,
-})
+    on_attach = on_attach,
+  })
+end
+return M
