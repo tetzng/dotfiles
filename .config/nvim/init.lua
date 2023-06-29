23bfcbb1758bc2068337d48c6de3bb7def8a2f63
Teppei Taguchi <tetzng.tt@gmail.com>
2023-06-30 03:13:54 +0900

    update vscode-neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,10 @@
-require("keymaps")
-require("options")
-
 if vim.g.vscode then
   require("vscode/keymaps")
+  vim.opt.clipboard = "unnamedplus" -- sync with system clipboard
 else
+  require("keymaps")
+  require("options")
+
   -- lazy.nvim
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   if not vim.loop.fs_stat(lazypath) then
--- .config/nvim/lua/vscode/keymaps.lua
+++ .config/nvim/lua/vscode/keymaps.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<C-p>", "<<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>>", { silent = true })
+-- vim.keymap.set("n", "<C-p>", "<<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>>", { silent = true })
+vim.keymap.set("i", "<C-j>", "<Nop>", { silent = true })
