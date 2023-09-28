eded9c7eae160497510caf8731bfbbb1e6bde84d
Teppei Taguchi <tetzng.tt@gmail.com>
2023-09-28 17:31:39 +0900

    fix

--- .config/nvim/lua/plugins/Comment.lua
+++ .config/nvim/lua/plugins/Comment.lua
@@ -5,8 +5,8 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
-    require("Comment").setup {
+    require("Comment").setup({
       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    }
+    })
   end,
 }
