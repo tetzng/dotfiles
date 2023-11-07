7a0b3414e8c43a9abe60abe2cc84f0060e81b80a
Teppei Taguchi <tetzng.tt@gmail.com>
2023-11-07 12:04:07 +0900

    chore(wezterm): update config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 local wezterm = require("wezterm")
-
-local config = {}
-if wezterm.config_builder then
-  config = wezterm.config_builder()
-end
+local act = wezterm.action
+local config = wezterm.config_builder()
 
 wezterm.on("gui-startup", function(cmd)
   local _tab, _pane, window = wezterm.mux.spawn_window(cmd or {})
@@ -24,15 +21,17 @@ config.font = wezterm.font_with_fallback({
   { family = "Cica" },
   { family = "Cica", assume_emoji_presentation = true },
 })
-config.font_size = 16.0
+config.font_size = 14
+config.cell_width = 0.9
+config.line_height = 1.2
 config.default_prog = { "/opt/homebrew/bin/fish", "-l" }
 config.color_scheme = "tokyonight_moon"
 config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 0.9
 
 config.window_padding = {
-  left = "1cell",
-  right = "1cell",
+  left = 0,
+  right = 0,
   top = 0,
   bottom = 0,
 }
@@ -48,55 +47,55 @@ config.keys = {
   {
     key = "%",
     mods = "LEADER",
-    action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+    action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
   },
   {
     key = '"',
     mods = "LEADER",
-    action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+    action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }),
   },
   -- move pane
-  { key = "h",          mods = "LEADER",      action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-  { key = "j",          mods = "LEADER",      action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-  { key = "k",          mods = "LEADER",      action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-  { key = "l",          mods = "LEADER",      action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+  { key = "h",          mods = "LEADER",      action = act({ ActivatePaneDirection = "Left" }) },
+  { key = "j",          mods = "LEADER",      action = act({ ActivatePaneDirection = "Down" }) },
+  { key = "k",          mods = "LEADER",      action = act({ ActivatePaneDirection = "Up" }) },
+  { key = "l",          mods = "LEADER",      action = act({ ActivatePaneDirection = "Right" }) },
   -- close pane
-  { key = "x",          mods = "LEADER",      action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+  { key = "x",          mods = "LEADER",      action = act({ CloseCurrentPane = { confirm = true } }) },
   -- new tab
-  { key = "t",          mods = "LEADER",      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-  { key = "c",          mods = "LEADER",      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+  { key = "t",          mods = "LEADER",      action = act({ SpawnTab = "CurrentPaneDomain" }) },
+  { key = "c",          mods = "LEADER",      action = act({ SpawnTab = "CurrentPaneDomain" }) },
   -- close tab
-  { key = "w",          mods = "LEADER",      action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+  { key = "w",          mods = "LEADER",      action = act({ CloseCurrentTab = { confirm = true } }) },
   -- move tab
-  { key = "LeftArrow",  mods = "LEADER",      action = wezterm.action({ ActivateTabRelative = -1 }) },
-  { key = "RightArrow", mods = "LEADER",      action = wezterm.action({ ActivateTabRelative = 1 }) },
-  { key = "H",          mods = "LEADER",      action = wezterm.action({ ActivateTabRelative = -1 }) },
-  { key = "L",          mods = "LEADER",      action = wezterm.action({ ActivateTabRelative = 1 }) },
-  { key = "1",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 0 }) },
-  { key = "2",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 1 }) },
-  { key = "3",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 2 }) },
-  { key = "4",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 3 }) },
-  { key = "5",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 4 }) },
-  { key = "6",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 5 }) },
-  { key = "7",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 6 }) },
-  { key = "8",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 7 }) },
-  { key = "9",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 8 }) },
-  { key = "0",          mods = "LEADER",      action = wezterm.action({ ActivateTab = 9 }) },
+  { key = "LeftArrow",  mods = "LEADER",      action = act({ ActivateTabRelative = -1 }) },
+  { key = "RightArrow", mods = "LEADER",      action = act({ ActivateTabRelative = 1 }) },
+  { key = "H",          mods = "LEADER",      action = act({ ActivateTabRelative = -1 }) },
+  { key = "L",          mods = "LEADER",      action = act({ ActivateTabRelative = 1 }) },
+  { key = "1",          mods = "LEADER",      action = act({ ActivateTab = 0 }) },
+  { key = "2",          mods = "LEADER",      action = act({ ActivateTab = 1 }) },
+  { key = "3",          mods = "LEADER",      action = act({ ActivateTab = 2 }) },
+  { key = "4",          mods = "LEADER",      action = act({ ActivateTab = 3 }) },
+  { key = "5",          mods = "LEADER",      action = act({ ActivateTab = 4 }) },
+  { key = "6",          mods = "LEADER",      action = act({ ActivateTab = 5 }) },
+  { key = "7",          mods = "LEADER",      action = act({ ActivateTab = 6 }) },
+  { key = "8",          mods = "LEADER",      action = act({ ActivateTab = 7 }) },
+  { key = "9",          mods = "LEADER",      action = act({ ActivateTab = 8 }) },
+  { key = "0",          mods = "LEADER",      action = act({ ActivateTab = 9 }) },
   -- zoom pane
-  { key = "z",          mods = "LEADER",      action = wezterm.action.TogglePaneZoomState },
+  { key = "z",          mods = "LEADER",      action = act.TogglePaneZoomState },
   -- search
-  { key = "f",          mods = "LEADER",      action = wezterm.action({ Search = { CaseInSensitiveString = "" } }) },
+  { key = "f",          mods = "LEADER",      action = act({ Search = { CaseInSensitiveString = "" } }) },
   -- copy mode
-  { key = "v",          mods = "LEADER",      action = wezterm.action.ActivateCopyMode },
+  { key = "v",          mods = "LEADER",      action = act.ActivateCopyMode },
   -- activate command palette
-  { key = "p",          mods = "LEADER|CTRL", action = wezterm.action.ActivateCommandPalette },
+  { key = "p",          mods = "LEADER|CTRL", action = act.ActivateCommandPalette },
   -- in copymode, open url with browser
-  { key = "o",          mods = "LEADER",      action = wezterm.action.OpenLinkAtMouseCursor },
+  { key = "o",          mods = "LEADER",      action = act.OpenLinkAtMouseCursor },
   -- open url with browser
   {
     key = "o",
     mods = "LEADER|CTRL",
-    action = wezterm.action.QuickSelectArgs({
+    action = act.QuickSelectArgs({
       label = "open url",
       patterns = {
         "https?://\\S+",
