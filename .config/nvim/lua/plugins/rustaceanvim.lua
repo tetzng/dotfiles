599e6638d5ad0b46249109505809c9be694cb4a7
Teppei Taguchi <tetzng.tt@gmail.com>
2024-01-14 01:00:24 +0900

    chore(rustaceanvim): update

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,8 +1,19 @@
 ---@class LazyPluginSpec
 local M = {
-  'mrcjkb/rustaceanvim',
-  version = '^3', -- Recommended
-  ft = { 'rust' },
+  "mrcjkb/rustaceanvim",
+  version = "^3", -- Recommended
+  ft = { "rust" },
+  config = function()
+    vim.g.rustaceanvim = {
+      server = {
+        on_attach = function(client, bufnr)
+          if client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(bufnr, true)
+          end
+        end,
+      },
+    }
+  end,
 }
 
 return M
