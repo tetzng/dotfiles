d35c821f41ac861cb80fbb5b2964587a4c53f0d4
Teppei Taguchi <tetzng.tt@gmail.com>
2024-06-03 21:42:19 +0900

    feat(ts-comments): Add plugin

---
+++ .config/nvim/lua/plugins/ts-comments.lua
@@ -0,0 +1,10 @@
+---@class LazyPluginSpec
+local M = {
+  -- "tetzng/ts-comments.nvim",
+  "folke/ts-comments.nvim",
+  -- branch = "add-rust",
+  opts = {},
+  event = "BufReadPost",
+}
+
+return M
