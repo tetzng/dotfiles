b7642cfcb2ef349c8de26462b7724a53ec76771e
Teppei Taguchi <tetzng.tt@gmail.com>
2025-02-01 20:28:29 +0900

    fix(fish): fix to do only on macOS

--- .config/fish/functions/update_fish_theme_by_system_appearance.fish
+++ .config/fish/functions/update_fish_theme_by_system_appearance.fish
@@ -1,4 +1,10 @@
 function update_fish_theme_by_system_appearance
+    set -l os (uname)
+
+    if test $os != "Darwin"
+        return
+    end
+
     if defaults read -g AppleInterfaceStyle 2>/dev/null | grep -q Dark
         fish_config theme choose Catppuccin\ Frappe
     else
