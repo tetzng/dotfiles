d7818d09b34bcde56e81b61acbc84f010302c0f4
Teppei Taguchi <tetzng.tt@gmail.com>
2025-02-16 23:30:00 +0900

    feat(neovim): add open github url under cursor

--- .config/nvim/lua/plugins/open-github-url.lua
+++ .config/nvim/lua/plugins/open-github-url.lua
@@ -2,6 +2,7 @@ return {
   "tetzng/open-github-url.nvim",
   keys = {
     { "<leader>gh", "<cmd>OpenGitHubUrlUnderCursor<cr>", desc = "Open GitHub URL under cursor" },
+    { "<leader>oo", "<cmd>OpenLinkUnderCursor<cr>",      desc = "Open URL or GitHub repo under cursor" },
   },
   config = true,
 }
