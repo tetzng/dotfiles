fa6617e90ab92b2851bd3f473579ce208ccb07f8
Teppei Taguchi <tetzng.tt@gmail.com>
2025-07-18 00:10:22 +0900

    chore(obsidian.nvim): update plugin repository path

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,6 +1,6 @@
 ---@class LazyPluginSpec
 local M = {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
