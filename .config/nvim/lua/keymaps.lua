d678b42c00b59b3309a0e888083eb96abdb3cdf9
Teppei Taguchi <tetzng.tt@gmail.com>
2025-07-18 00:00:09 +0900

    chore(nvim): remove unused key mappings

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,13 +2,11 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
--- vim.keymap.set("n", "<leader>t", ":tabnew<CR>", { silent = true, desc = "open new tab" })
-vim.keymap.set("n", "gr", "gT", { silent = true })
 
 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
 vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>")
 vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>")
-vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+-- vim.keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
 vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 vim.keymap.set("n", "go", "<cmd>lua vim.diagnostic.open_float()<CR>")
